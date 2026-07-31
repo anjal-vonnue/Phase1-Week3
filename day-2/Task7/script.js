3d309c755428a52d398684a1c0dfbbfe12b91ef2
anjal-vonnue <anjal.biju@vonnue.com>
2026-07-31 16:09:45 +0530

    feat: built scroll animation to the index page, added a back to top button and also implemented a progress bar to the index page

---
+++ day-2/Task7/script.js
@@ -0,0 +1,41 @@
+const animationTarget = document.querySelectorAll(".animation-task");
+
+const observer = new IntersectionObserver((entries) => {
+  //   console.log("entries", entries);
+
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log(entry.target);
+      entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
+    }
+  });
+});
+
+for (let i = 0; i < animationTarget.length; i++) {
+  observer.observe(animationTarget[i]);
+}
+
+const backToTop = document.querySelector(".backToTop");
+
+window.addEventListener("scroll", () => {
+  requestAnimationFrame(progressBarUpdate);
+  if (scrollY <= 300) {
+    backToTop.style.display = "none";
+  } else {
+    backToTop.style.display = "block";
+  }
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+const innerBar = document.querySelector(".inner-bar");
+
+function progressBarUpdate() {
+  const totalScroll = document.body.scrollHeight - window.innerHeight;
+  const progress = Math.floor((scrollY / totalScroll) * 100);
+  innerBar.style.width = `${progress}%`;
+}
