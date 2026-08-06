f474a5a2c6a55e20ff1d84a42af5316ec7a68b96
anjal-vonnue <anjal.biju@vonnue.com>
2026-08-06 11:51:27 +0530

    feat: built a five step pipeline for csv parsing

--- day-3/Task3/functions.js
+++ day-3/Task3/functions.js
@@ -15,14 +15,14 @@ function updateUserImpure(users, id, changes) {
   return users;
 }
 
-console.log("before impure:", users);
+// console.log("before impure:", users);
 
 const impureResult = updateUserImpure(users, 2, {
   name: "ramos",
   position: "defender",
 });
 
-console.log("after impure:", users);
+// console.log("after impure:", users);
 
 function updateUserPure(users, id, changes) {
   const newUser = users.map((u) => {
@@ -35,15 +35,15 @@ function updateUserPure(users, id, changes) {
   return newUser;
 }
 
-console.log("before pure:", users);
+// console.log("before pure:", users);
 
 const pureResult = updateUserPure(users, 3, {
   name: "zidane",
   position: "legend",
 });
-console.log("after pure:", users);
+// console.log("after pure:", users);
 
-console.log("pure result: ", pureResult);
+// console.log("pure result: ", pureResult);
 
 //deep freeze https://dev.to/syed_ammar/javascript-object-deep-freeze-vs-shallow-freeze-4nk8
 const freezeObject = {
@@ -61,11 +61,93 @@ function deepFreeze(obj) {
   }
   return Object.freeze(obj);
 }
-console.log("before: ", freezeObject);
+// console.log("before: ", freezeObject);
 
 deepFreeze(freezeObject);
 
 freezeObject.name = "Ronaldo";
 freezeObject.details.age = 30;
 
-console.log("after: ", freezeObject);
+// console.log("after: ", freezeObject);
+
+//csv pipeline
+
+const csv = `"anjal", 22
+"ronaldo", 31
+"kroos", 30
+"pele", 50
+`;
+
+function parseCSV(csv) {
+  let csvTrim = csv.trim();
+  let csvSplit = csvTrim.split("\n");
+  let newArr = csvSplit.map((r) => {
+    // console.log(r.split(","));
+
+    const [name, age] = r.split(",");
+    return { name, age };
+  });
+  return newArr;
+}
+
+function validateRows(csv) {
+  // console.log(csv);
+
+  const newArr = csv.map((r) => {
+    if (Number(r.age > 30)) {
+      return {
+        ...r,
+        valid: true,
+      };
+    } else {
+      return {
+        ...r,
+        valid: false,
+      };
+    }
+  });
+
+  return newArr;
+}
+
+function transformRows(csv) {
+  const newArr = csv.map((r) => {
+    return {
+      ...r,
+      name: r.name.replace(/"/g, "").toUpperCase(),
+      age: Number(r.age),
+    };
+  });
+
+  // console.log(newArr);
+
+  return newArr;
+}
+
+function filterInvalid(csv) {
+  const newArr = csv.filter((r) => {
+    if (r.valid) {
+      return r;
+    }
+  });
+
+  // console.log(newArr);
+  return newArr;
+}
+
+function formatOutput(csv) {
+  // console.log(csv);
+
+  const newArr = csv.map((r) => {
+    return `${r.name} is above 30 and the actual age is ${r.age}`;
+  });
+
+  return newArr;
+}
+
+const parsedCSV = parseCSV(csv);
+const validatedRows = validateRows(parsedCSV);
+const transformedRows = transformRows(validatedRows);
+const filteredRows = filterInvalid(transformedRows);
+const output = formatOutput(filteredRows);
+console.log(output);
